7d588e30f453aa81c9712e96e5eb0e8000316b49
Ryan Miller <rmill@blacklinegps.com>
2020-02-09 12:50:37 -0700

    updated firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,9 +8,10 @@ export const environment = {
     apiKey: "AIzaSyCdZ_eDSfZ_R8VEAJQEzzyei5y2ss4VGbA",
     authDomain: "drink-up-cinema.firebaseapp.com",
     databaseURL: "https://drink-up-cinema.firebaseio.com",
-    messagingSenderId: "828526569727",
     projectId: "drink-up-cinema",
-    storageBucket: "drink-up-cinema.appspot.com"
+    storageBucket: "drink-up-cinema.appspot.com",
+    messagingSenderId: "828526569727",
+    appId: "1:828526569727:web:7fa45208cddef659e204ca"
   },
   production: false
 };
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,15 +2,17 @@ self.importScripts('https://www.gstatic.com/firebasejs/5.4.0/firebase-app.js');
 self.importScripts('https://www.gstatic.com/firebasejs/5.4.0/firebase-messaging.js');
 
 // TODO: This should be read from the config file
-let config =  {
+const firebaseConfig = {
   apiKey: "AIzaSyCdZ_eDSfZ_R8VEAJQEzzyei5y2ss4VGbA",
   authDomain: "drink-up-cinema.firebaseapp.com",
-  messagingSenderId: "828526569727",
+  databaseURL: "https://drink-up-cinema.firebaseio.com",
   projectId: "drink-up-cinema",
-  storageBucket: "drink-up-cinema.appspot.com"
-}
+  storageBucket: "drink-up-cinema.appspot.com",
+  messagingSenderId: "828526569727",
+  appId: "1:828526569727:web:7fa45208cddef659e204ca"
+};
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 try { const messaging = firebase.messaging() }
 catch(err) { console.log(err) }
